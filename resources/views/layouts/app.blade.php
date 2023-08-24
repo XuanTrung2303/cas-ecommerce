3baf76173032112322d817f741ad052378af7f44
Xuan Trung <xuantrung276@gmail.com>
2023-08-24 16:41:42 +0700

    Part7

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
                 style="width: 160px;height:100px"></a>
         <div class="text-2xl relative">
             <a href="{{ route('wishlist') }}"><i class='bx bx-heart'></i></a>
-            <i class='bx bx-user'></i>
+            <a href="{{ route('account') }}"><i class='bx bx-user'></i></a>
             <a href="{{ route('cart') }}"><i class='bx bx-cart'></i></a>
             <span
                 class="absolute top-0 -right-2.5 bg-indigo-600 rounded-full w-4 h-4 text-xs text-white text-center">0</span>
