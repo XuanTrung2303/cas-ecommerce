7bf95cb43f606504068074954cc578a372463533
Xuan Trung <xuantrung276@gmail.com>
2023-08-24 17:40:46 +0700

    Part9

---
+++ app/Models/Variant.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Variant extends Model
+{
+    use HasFactory;
+
+    protected $table = 'variants';
+
+    protected $fillable = [
+        'product_id',
+        'color_id',
+        'size_id',
+        'sku',
+        'mrp',
+        'selling_price',
+        'stock'
+    ];
+}
